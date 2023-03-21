0d8f1a79e5edbc5cfca232140191dabcd526e067
nav-j0t <navjotsinghbedi2001@gmail.com>
2023-03-20 21:44:39 -0700

    Setting up all the Header.js links using <link> and moving to load products from Products.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Navbar, Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          {/* <LinkContainer></LinkContainer> */}
-          <Navbar.Brand href='/'>React_Shop</Navbar.Brand>
+          <Link to='/'>
+            <Navbar.Brand>React_Shop</Navbar.Brand>
+          </Link>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
+              <Nav.Link as={Link} to='/cart'>
                 <i className='fas fa-shopping-cart mx-2'></i>
                 Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
+
+              <Nav.Link as={Link} to='/login'>
                 <i className='fas fa-user mx-2'></i>
                 Sign In
               </Nav.Link>
